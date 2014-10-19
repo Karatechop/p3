ed759ad0eca4be33a46e98625f6bb07631867311
Karatechop <boris.rugel@gmail.com>
2014-10-19 11:56:13 +0200

    custom 404 page

--- app/start/global.php
+++ app/start/global.php
@@ -51,6 +51,11 @@ App::error(function(Exception $exception, $code)
 	Log::error($exception);
 });
 
+App::missing(function($exception)
+{
+	return Response::make("Page not found", 404);
+});
+
 /*
 |--------------------------------------------------------------------------
 | Maintenance Mode Handler
